3caf28eda16ead9108877feafc194d5e2573b839
Mahmoud Ramadan <mahmoudr03@gmail.com>
2018-10-26 02:26:08 +0200

    scroll

--- js/main.js
+++ js/main.js
@@ -1,34 +1,40 @@
 $(function () {
-    $(".fa-search").click(function(){
+    $(".fa-search").click(function () {
         $("input").fadeToggle()
     });
     // open video
     $(".js-video-button").modalVideo({
-			youtube:{
-				controls:0,
-				nocookie: true
-			}
-        });
-        
-       $(".fa-angle-down").click(function () {
-           $('html, body').animate({
-               scrollTop: $("#slide").offset().top
-           }, 800);
-       });
-
-   
-    $('.home').ripples({
-        resolution:312,
-        dropRadius: 20,
-        perturbance: 0.01,
-        interactive: true,  
+        youtube: {
+            controls: 0,
+            nocookie: true
+        }
+    });
+    $(".scroll span").click(function () {
+        $('html, body').animate({
+            scrollTop: $(".slide").offset().top
+        }, 700);
     });
 
-   
-        $('#fullpage').fullpage({
-            //options here
-            autoScrolling: true,
-            scrollHorizontally: true
-        });
+    $(".fa-angle-down").click(function () {
+        $('html, body').animate({
+            scrollTop: $(".slide").offset().top
+        }, 700);
+    });
+    
+
+    // ripples water 
+    $('.home').ripples({
+        resolution: 312,
+        dropRadius: 20,
+        perturbance: 0.01,
+        interactive: true,
+    });
+
+
+    $('#fullpage').fullpage({
+        //options here
+        autoScrolling: true,
+        scrollHorizontally: true
+    });
 
 });
