c539d0bcc37f1f22c4bb663ecb8eb7c56488dbfc
Mahmoud Ramadan <mahmoudr03@gmail.com>
2018-10-29 14:21:38 +0200

    boxing

--- js/main.js
+++ js/main.js
@@ -30,5 +30,31 @@ $(function () {
 
     // scroll
     $('#fullpage').fullpage();
-
+    
+    // lighte slider
+    $('#light-slider').lightSlider({
+        item: 1,
+        loop: false,
+        slideMove: 1,
+        easing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+        speed: 600,
+        slideMargin: 0,
+        responsive: [
+            {
+                breakpoint: 800,
+                settings: {
+                    item: 3,
+                    slideMove: 1,
+                    slideMargin: 6,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    item: 2,
+                    slideMove: 1
+                }
+            }
+        ]
+    });  
 });
